9b4bc9a4d201b6ee46c9025e2122fc43ca50dc78
Khizra Ali <50247649+khizra-ali@users.noreply.github.com>
2019-05-18 21:21:21 +0500

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,6 @@
+pi=float(input("Enter value"))
+print(pi)
+radius=float(input("Enter value"))
+print(radius)
+area=pi*radius**2
+print(area)
---
+++ 2.py
@@ -0,0 +1,8 @@
+#check if number is positve, negative or zero
+num=int(input("enter number: "))
+if num >= 1:
+    print("positive number")
+elif num < 0:
+    print("negative number")
+else:
+    print("zero")
